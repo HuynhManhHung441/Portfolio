54a22930f6ddf6ca2be9e919f67b79978fec730c
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2024-10-17 20:52:14 +0700

    array

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,19 @@ function showAleart() {
   alert("Go on!");
 }
 
+
+const skills = ["HTML", "CSS", "JavaScript"];
+
+
+const skillsList = document.getElementById("skills");
+  
+skills.forEach(skill => {
+  const skillItem = document.createElement("li");
+  skillItem.textContent = skill;
+  skillsList.appendChild(skillItem);
+});
+
+
 let isCode = true;
 let totalDays = 1;
 
